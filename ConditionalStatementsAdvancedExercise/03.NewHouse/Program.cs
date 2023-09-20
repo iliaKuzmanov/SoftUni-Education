7d27fc3492e4b1b20ac267ae2c37b51767eb5523
iliaKuzmanov <145570443+iliaKuzmanov@users.noreply.github.com>
2023-09-20 16:10:16 +0300

    Add files via upload

---
+++ ConditionalStatementsAdvancedExercise/03.NewHouse/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace _03.NewHouse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            int cnt = int.Parse(Console.ReadLine());
+            int budget = int.Parse(Console.ReadLine());
+            double price = 0;
+            double neededMoney = 0;
+            switch (type)
+            {
+                case "Roses":
+                    price = cnt * 5;
+                    if (cnt>80)
+                    {
+                        neededMoney = price * 0.9;
+                    }
+                    else
+                    {
+                        neededMoney = price;
+                    }
+                    break;
+                case "Dahlias":
+                    price = cnt * 3.8;
+                    if (cnt>90)
+                    {
+                        neededMoney = price * 0.85;
+                    }
+                    else
+                    {
+                        neededMoney = price;
+                    }
+                    break;
+                case "Tulips":
+                    price = 2.8 * cnt;
+                    if (cnt>80)
+                    {
+                        neededMoney = price * 0.85;
+                    }
+                    else
+                    {
+                        neededMoney = price;
+                    }
+                    break;
+                case "Narcissus":
+                    price = cnt * 3;
+                    if (cnt<120)
+                    {
+                        neededMoney = price * 1.15;
+                    }
+                    else
+                    {
+                        neededMoney = price;
+                    }
+                    break;
+                case "Gladiolus":
+                    price = cnt * 2.5;
+                    if (cnt<80)
+                    {
+                        neededMoney = price * 1.2;
+                    }
+                    else
+                    {
+                        neededMoney = price;
+                    }
+                    break;
+                    
+            }
+            if (neededMoney <= budget)
+            {
+                Console.WriteLine($"Hey, you have a great garden with {cnt} {type} and {(budget - neededMoney):f2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money, you need {(neededMoney - budget):f2} leva more.");
+            }
+        }
+    }
+}
---
+++ ConditionalStatementsAdvancedExercise/05.Journey/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _05.Journey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine(); //winter,summer
+           string tripType="";
+            string place = "";
+            double moneySpent = 0;
+            if (budget<=100)
+            {
+                tripType = "Bulgaria";
+                if (season=="winter")
+                {
+                    moneySpent = budget * 0.7;
+                    place = "Hotel";
+                }
+                else
+                {
+                    moneySpent = budget * 0.3;
+                    place = "Camp";
+                }
+            }
+            else if (budget<=1000)
+            {
+                tripType = "Balkans";
+                if (season == "winter")
+                {
+                    moneySpent = budget * 0.8;
+                    place = "Hotel";
+                }
+                else
+                {
+                    moneySpent = budget * 0.4;
+                    place = "Camp";
+                }
+            }
+            else
+            {
+                tripType = "Europe";
+                moneySpent = budget * 0.9;
+                place = "Hotel";
+            }
+            Console.WriteLine($"Somewhere in {tripType}");
+            Console.WriteLine($"{ place} - { moneySpent:f2}");
+        }
+    }
+}
